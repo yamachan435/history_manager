073d9946bdf284fe9790bb0320104e4ab7a76377
vagrant <vagrant@localhost.localdomain>
2019-05-29 11:45:20 +0000

    Added Idm.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_112027) do
+ActiveRecord::Schema.define(version: 2019_05_29_113324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 2019_05_22_112027) do
     t.bigint "zaim_id"
     t.integer "user_id"
     t.index ["user_id"], name: "index_histories_on_user_id"
+  end
+
+  create_table "idms", force: :cascade do |t|
+    t.string "idm"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_idms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
