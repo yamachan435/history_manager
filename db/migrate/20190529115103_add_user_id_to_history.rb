79ef5994ecf93fb2990d7a03d569175417455c6f
vagrant <vagrant@localhost.localdomain>
2019-06-04 15:35:35 +0000

    Revert "Temporarily migration modified."
    This reverts commit aacc1d9700c3a6f1a8573b8edea4f8ae221eb042.

--- db/migrate/20190529115103_add_user_id_to_history.rb
+++ db/migrate/20190529115103_add_user_id_to_history.rb
@@ -1,5 +1,5 @@
 class AddUserIdToHistory < ActiveRecord::Migration[5.2]
   def change
-#    add_reference :histories, :user, foreign_key: true
+    add_reference :histories, :user, foreign_key: true
   end
 end
